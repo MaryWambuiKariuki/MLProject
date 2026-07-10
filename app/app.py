99d01a4a4f35691433d883932607c784971ec66d
bobostarr10 <marywambui10122004@gmail.com>
2026-07-10 10:29:10 +0300

    Updated content

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 import streamlit as st
-import joblib
-import pandas as pd
 
 st.set_page_config(
     page_title="House Price Prediction",
     layout="wide"
 )
 
-st.title("House Price Prediction")
-st.write(
-    """
-    Predict the selling price of a house using a machine learning model trained on the Ames Housing dataset.
-    """
-)
+st.title(" House Price Prediction")
+
+st.write("""
+Welcome to the **House Price Prediction** application.
+
+This application uses a **Gradient Boosting Regression Model**
+trained on the Ames Housing Dataset.
+""")
