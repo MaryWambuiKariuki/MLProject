33290fd0a992d883096e5c83d5d4da53a3d7beb9
bobostarr10 <marywambui10122004@gmail.com>
2026-07-10 10:05:43 +0300

    updated content

--- app/app.py
+++ app/app.py
@@ -4,11 +4,10 @@ import pandas as pd
 
 st.set_page_config(
     page_title="House Price Prediction",
-    page_icon="🏠",
     layout="wide"
 )
 
-st.title("🏠 House Price Prediction")
+st.title("House Price Prediction")
 st.write(
     """
     Predict the selling price of a house using a machine learning model trained on the Ames Housing dataset.
